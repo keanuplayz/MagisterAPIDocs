44522d6b43d9920159da6a03ca98f2870f586413
Keanu <keanutimmermans8@gmail.com>
2021-04-13 18:45:19 +0200

    Improved `/api/rooster` docs with comments

--- magister-api.ts
+++ magister-api.ts
@@ -43,52 +43,52 @@
  *   {
  *     "Items": [
  *       {
- *         "Aantekening": string,
- *         "Afgerond": boolean,
- *         "Bijlagen": <type unknown, probably ObjectArray>,
- *         "Docenten": [
+ *         "Aantekening": string, // Comment from teacher
+ *         "Afgerond": boolean, // Has finished homework?
+ *         "Bijlagen": [ { ... } ], // ObjectArray of attachments
+ *         "Docenten": [ // ObjectArray of teachers
  *           {
- *             "Docentcode": string,
- *             "Id": number,
- *             "Naam": string
+ *             "Docentcode": string, // Teacher abbreviation
+ *             "Id": number, // ???
+ *             "Naam": string // Full teacher name
  *           }
  *         ],
- *         "DuurtHeleDag": boolean,
- *         "Einde": string,
- *         "Groepen": <type unknown, probably ObjectArray>,
- *         "HeeftBijlagen": boolean,
- *         "HerhaalStatus": number,
- *         "Herhaling": <type unknown>,
- *         "Id": number,
- *         "InfoTyoe": number,
+ *         "DuurtHeleDag": boolean, // Does the appointment last the whole day?
+ *         "Einde": Date, // Date of when the class ends (Full `Date` string e.g. "2021-04-13T07:35:00.0000000Z")
+ *         "Groepen": [ { ... } ], // ObjectArray of... "groups"?
+ *         "HeeftBijlagen": boolean, // Does the appointment have any attachments?
+ *         "HerhaalStatus": number, // ???
+ *         "Herhaling": <type unknown>, // ???
+ *         "Id": number, // ID of appointment?
+ *         "InfoType": number, // ???
  *         "Inhoud": <type unknown>
- *         "IsOnlineDeelname": boolean,
- *         "LesuurTotMet": number,
- *         "LesuurVan": number,
- *         "Links": [
+ *         "IsOnlineDeelname": boolean, // Is the class online?
+ *         "LesuurTotMet": number, // ???
+ *         "LesuurVan": number, // ???
+ *         "Links": [ // ObjectArray with Magister API URLs
  *              {
- *                  "Href": string,
- *                  "Rel": string
+ *                  "Href": string, // API URL
+ *                  "Rel": string // "Self" or "Next"
  *              }
  *          ],
  *          "Lokalen": [
  *              {
- *                  "Naam": string
+ *                  "Naam": string // e.g. "s426"
  *              }
  *          ],
- *          "Lokatie": string,
- *          "Omschrijving": string,
+ *          "Lokatie": string, // e.g. Exactly the same thing as above, e.g. "s426"
+ *          "Omschrijving": string, // Description of class as displayed in "Omschrijving" on appointment details in Magister
  *          "OpdrachtId": number,
- *          "Start": string,
- *          "Status": number,
- *          "Type": number,
+ *          "Start": Date, // Date of when the class starts (Full `Date` string e.g. "2021-04-13T07:05:00.0000000Z")
+ *          "Status": number, // ???
+ *          "Type": number, // ???
  *          "Vakken": [
  *              {
  *                  "Id": number,
- *                  "Naam": string
+ *                  "Naam": string // Name as displayed under "Vak" on appointment details in Magister
  *              }
  *          ],
- *          "WeergaveType": number
+ *          "WeergaveType": number // ???
  *       }
  *     ]
  *   }
